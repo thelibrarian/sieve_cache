8a57314cec414e2648d81722db06579bd13fb806
tony <tony@agrav.net>
2024-01-05 10:42:02 +0000

    Moved class to be more idiomatic
    FossilOrigin-Name: 82021bb80172959b70e141c9dc0fb20edbe5cde55145846d8a52f5aa5c9eef05

---
+++ lib/sieve_cache/cache.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+module SieveCache
+  # A thread-safe general-purpose fixed-size cache that uses the SIEVE eviction
+  # algorithm for evicting old cache items.
+  #
+  # Works much like a Hash where items are looked up by key, returning `nil` if
+  # there is a cache miss.
+  class Cache
+    Node = Struct.new('Node', :key, :value, :visited, :prev, :next)
+
+    def initialize(capacity)
+      @capacity = capacity
+      @lookup = {}
+      @head = nil
+      @tail = nil
+      @hand = nil
+    end
+
+    def store(key, value)
+      raise StandardError, 'Unable to store item, cache is full' if size >= @capacity
+
+      n = Node.new(key: key, value: value, visited: false, next: @head)
+      @head.prev = n unless @head.nil?
+      @head = n
+      if @tail.nil?
+        @tail = n
+        @hand = n
+      end
+      @lookup[key] = n
+    end
+
+    def fetch(key, &block)
+      return nil unless @lookup.key?(key) || block_given?
+
+      node = @lookup[key]
+      if node.nil?
+        evict
+        node = store(key, block.call)
+      else
+        node.visited = true
+      end
+      node.value
+    end
+
+    def size
+      @lookup.size
+    end
+
+    alias count size
+
+    private
+
+    def evict
+      while @hand&.visited
+        @hand.visited = false
+        @hand = @hand.prev || @tail
+      end
+      delist_hand
+      @lookup.delete(@hand.key) unless @hand.nil?
+      @hand = @hand&.prev
+    end
+
+    def delist_hand
+      if @hand.prev
+        @hand.prev.next = @hand.next
+      else
+        @head = @hand.next
+      end
+      if @hand.next
+        @hand.next.prev = @hand.prev
+      else
+        @tail = @hand.prev
+      end
+    end
+  end
+end
